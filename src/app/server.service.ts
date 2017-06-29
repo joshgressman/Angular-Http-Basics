e8166595389d4c59335293661802cb60bd848f07
joshgressman <joshgressman@gmail.com>
2017-06-29 15:14:55 -0500

    added get

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
+import { Http, Headers } from '@angular/http'
 
 @Injectable() //used when injecting a service into a service
 export class ServerService {
 
   constructor(private http: Http){} //http enables angualr to send requests
 
+  //Post servers to firebase server
   storeServers(servers: any[]){
-    return this.http.post('https://udemy-ng-http-a4fbf.firebaseio.com/data.json', servers);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post('https://udemy-ng-http-a4fbf.firebaseio.com/data.json', servers, {headers: headers});
   }
+
+  //Get data from firebase DB
+  getServers(){
+    return this.http.get('https://udemy-ng-http-a4fbf.firebaseio.com/data.json');
+  }
+
 }
